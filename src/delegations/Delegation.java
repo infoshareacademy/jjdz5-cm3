02f71304f20a144b20d16a03b42308fcb1d275fe
kurowskicisa <kurowskic.isa@gmail.com>
2018-07-09 22:41:47 +0200

    mikro zmiana na dziś

--- src/delegations/Delegation.java
+++ src/delegations/Delegation.java
@@ -9,14 +9,17 @@ public class Delegation {
     private String status ;
     private String startPoint ;
 
+    
     /*Obiekty z klas zawierających pola i metody */
-    private Employee employee = new Employee();
-    private Destination destination = new Destination();
-    private AddDelegation addDelegation = new AddDelegation();
-    private AcceptDelegation acceptDelegation = new AcceptDelegation();
+    private Employee          employee          = new Employee();
+    private Destination       destination       = new Destination();
+    private AddDelegation     addDelegation     = new AddDelegation();
+    private AcceptDelegation  acceptDelegation  = new AcceptDelegation();
     private DiscardDelegation discardDelegation = new DiscardDelegation();
     private PreviewDelegation previewDelegation = new PreviewDelegation();
 
+    public Delegation() {
+    }
 
 
 }
