8d6086496d6285085b458f96f9b95500dd095b30
kurowskicisa <kurowskic.isa@gmail.com>
2018-07-10 22:49:26 +0200

    Nowy kod (konstruktory, gettery, settery) w klasach: Delegation, Destination, Employee, PreviewDelegtion i DelegationMenu.

--- src/delegations/PreviewDelegation.java
+++ src/delegations/PreviewDelegation.java
@@ -2,7 +2,14 @@ package delegations;
 
 public class PreviewDelegation {
 
-    public void previewDelegation() {
-        System.out.println("przegladanie delegacji");
+    public void previewDelegation(Delegation myDelegation) {
+        String previewEmployee = "";
+        System.out.println("przegladanie delegacji test");
+
+        myDelegation.setEmployee( new Employee ("Michał" , "Michalek"));
+        System.out.println (myDelegation.getEmployee ().getEmployeeName () );
+
+
+
     }
 }
