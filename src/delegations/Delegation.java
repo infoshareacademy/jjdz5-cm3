1afd5671889065246378df0909a68c7ff0c3dc03
kurowskicisa <kurowskic.isa@gmail.com>
2018-07-11 23:04:43 +0200

    Zmiany w: Delegation, Destination, Employee, PrevievDelegation.

--- src/delegations/Delegation.java
+++ src/delegations/Delegation.java
@@ -11,15 +11,44 @@ public class Delegation {
 
 
     /*Obiekty z klas zawierających pola i metody */
-    private Employee          employee          = new Employee();
-    private Destination       destination       = new Destination();
-    private AddDelegation     addDelegation     = new AddDelegation();
-    private AcceptDelegation  acceptDelegation  = new AcceptDelegation();
-    private DiscardDelegation discardDelegation = new DiscardDelegation();
-    private PreviewDelegation previewDelegation = new PreviewDelegation();
+    private Employee employee = new Employee();
+    private Destination destination = new Destination();
+  //  private AddDelegation addDelegation = new AddDelegation();
+  //  private AcceptDelegation acceptDelegation = new AcceptDelegation();
+   // private DiscardDelegation discardDelegation = new DiscardDelegation();
+  //  private PreviewDelegation previewDelegation = new PreviewDelegation();
 
     public Delegation() {
     }
 
 
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public Destination getDestination() {
+        return destination;
+    }
+
+    public void setDestination(Destination destination) {
+        this.destination = destination;
+    }
+
+    @Override
+    public String toString() {
+        return "Delegation{" +
+                "creationDate='" + creationDate + '\'' +
+                ", startDate='" + startDate + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", purpose='" + purpose + '\'' +
+                ", status='" + status + '\'' +
+                ", startPoint='" + startPoint + '\'' +
+                ", employee=" + employee +
+                ", destination=" + destination +
+                '}' + '\n';
+    }
 }
--- src/delegations/PreviewDelegation.java
+++ src/delegations/PreviewDelegation.java
@@ -3,13 +3,6 @@ package delegations;
 public class PreviewDelegation {
 
     public void previewDelegation(Delegation myDelegation) {
-        String previewEmployee = "";
-        System.out.println("przegladanie delegacji test");
-
-        myDelegation.setEmployee( new Employee ("Michał" , "Michalek"));
-        System.out.println (myDelegation.getEmployee ().getEmployeeName () );
-
-
-
     }
+
 }
