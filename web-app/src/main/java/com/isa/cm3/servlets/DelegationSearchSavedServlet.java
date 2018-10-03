0f20cf73e5f17618eba471d67e3e5fb14b9f8e56
kurowskicisa <kurowskic.isa@gmail.com>
2018-10-03 22:35:22 +0200

    End of day: 2018.10.03

--- web-app/src/main/java/com/isa/cm3/servlets/DelegationSearchSavedServlet.java
+++ web-app/src/main/java/com/isa/cm3/servlets/DelegationSearchSavedServlet.java
@@ -8,6 +8,7 @@ import com.isa.cm3.freemarker.MapModelGenerator;
 import com.isa.cm3.freemarker.TemplateProvider;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
+
 import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,11 +17,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.stream.Collectors;
 
-@WebServlet(urlPatterns = "/searchDelegations")
-public class DelegationSearchServlet extends HttpServlet {
+@WebServlet(urlPatterns = "/searchDelegationsSaved")
+public class DelegationSearchSavedServlet extends HttpServlet {
 
     @Inject
     private TemplateProvider templateProvider;
@@ -34,28 +34,15 @@ public class DelegationSearchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        String wybor = " ";
-
         resp.setHeader ("Content-Type", "text/html; charset=UTF-8");
         resp.setContentType ("text/html;charset=UTF-8 pageEncoding=\"UTF-8");
 
         delegationsLoadFromFile.loadDelegationsFromFile ();
 
-        wybor = req.getParameter ("status");
 
-        System.out.println ("doGet switch " + wybor);
-        if (wybor != null) {
-            switch (wybor) {
-                case "SAVED":
-                    System.out.println ("doGet wybor " + wybor);
-                    break;
-                default:
-                    System.out.println ("doGet wybor default " + wybor);
-                    break;
-            }
-        }
             mapModelGenerator.setModel ("delegations", delegationRepository.getList ().stream ()
                     .sorted (Comparator.comparingInt (Delegation::getFileLineNumber))
+                    .filter(delegation -> delegation.getDelegationStatus().equals(DelegationStatus.SAVED))
                     .collect (Collectors.toList ()));
 
             Template template = templateProvider.getTemplate (getServletContext (), "searchingDelegationsTemplate");
