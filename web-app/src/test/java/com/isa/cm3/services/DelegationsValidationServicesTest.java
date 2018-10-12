7fb09216109dfb0c25df9be072b025d45317de13
Marek Kalkowski <kontakt@marekkalkowski.pl>
2018-10-12 12:51:42 +0200

    testy jednostkowe - cd.

--- web-app/src/test/java/com/isa/cm3/services/DelegationsValidationServicesTest.java
+++ web-app/src/test/java/com/isa/cm3/services/DelegationsValidationServicesTest.java
@@ -1,13 +1,23 @@
 package com.isa.cm3.services;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DelegationsValidationServicesTest {
 
+    @BeforeAll
+    Map<String,String> map = new HashMap<>();
+
+
     @Test
-    void shouldReturnTrueWhenStartDateisAfterEndDate{
+    void shouldReturnTrueWhenStartDateisAfterEndDate(){
 
     }
 
